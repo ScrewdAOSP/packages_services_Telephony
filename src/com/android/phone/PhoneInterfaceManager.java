9486a28f50ef1a5c3d27b2dd42b498d9f24cc10d
Ricardo Cerqueira <github@cerqueira.org>
2012-04-17 01:34:31 +0100

    Telephony: Enable LTE for GSM LTE phones (part I)
    Change-Id: I580c37f3272c8cc8363e8db5b1c227bf6f74a04a
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -3431,4 +3431,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             phone.setPolicyDataEnabled(enabled);
         }
     }
+
+    public int getLteOnGsmMode() {
+        return mPhone.getLteOnGsmMode();
+    }
 }
